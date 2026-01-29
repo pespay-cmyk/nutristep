bbbc1842e1ae6fd703507ed2e75d1041e4681280
pespay-cmyk <pespay@gmail.com>
2026-01-29 14:24:26 +0100

    Update app.run to use PORT environment variable
    Change app.run to use environment variable for port and disable debug mode.

--- app.py
+++ app.py
@@ -354,4 +354,5 @@ def create_tables():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
